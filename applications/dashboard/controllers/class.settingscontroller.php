5806f013328c03b7bdaa56514d716c0d470c1a61
Todd Burry <todd@vanillaforums.com>
2016-04-11 18:31:43 -0400

    Remove some events.
    The SettingsController event was never firing because it wasn’t being called correctly. Less events are good.

--- applications/dashboard/controllers/class.settingscontroller.php
+++ applications/dashboard/controllers/class.settingscontroller.php
@@ -1395,8 +1395,6 @@ class SettingsController extends DashboardController {
                 }
 
                 saveToConfig($ConfigSaveData);
-                $this->fireEvent['AfterSaveThemeOptions'];
-
                 $this->informMessage(t("Your changes have been saved."));
             }
 
